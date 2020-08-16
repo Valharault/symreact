7e113f30b94ae4dc333e00118d79411dab36bb36
valharault <valentinharault1@gmail.com>
2020-08-16 17:44:27 +0200

    Mise en place de l'application & bdd

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Customer;
+use App\Entity\Invoice;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+
+    /**
+     * L'encodeur de mot de passe
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+
+        for ($u=0;$u<10;$u++){
+            $chrono = 1;
+
+            $user = (new User())
+                ->setEmail($faker->email)
+                ->setLastName($faker->lastName)
+                ->setFirstName($faker->firstName);
+            $user = $user->setPassword($this->encoder->encodePassword($user,'password'));
+            $manager->persist($user);
+
+            for ($c=0;$c<mt_rand(5,20);$c++){
+                $customer = (new Customer())
+                    ->setFirstName($faker->firstName)
+                    ->setLastName($faker->lastName)
+                    ->setCompany($faker->company)
+                    ->setEmail($faker->email)
+                    ->setUser($user)
+                ;
+                $manager->persist($customer);
+
+                for ($i=0;$i< mt_rand(3,10);$i++){
+                    $invoice = (new Invoice())
+                        ->setAmount($faker->randomFloat(2,250,5000))
+                        ->setCustomer($customer)
+                        ->setSendAt($faker->dateTimeBetween('-6 month'))
+                        ->setStatus($faker->randomElement(['SENT','PAID','CANCELLED']))
+                        ->setChrono($chrono)
+                    ;
+                    $chrono++;
+                    $manager->persist($invoice);
+                }
+            }
+        }
+
+        $manager->flush();
+    }
+}
